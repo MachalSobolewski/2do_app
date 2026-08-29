8081a6acb59cb0b4c2b37a50dcf76067fd6a41b6
Michał Sobolewski <sobolewski.michal@outlook.com>
2026-08-30 00:56:18 +0200

    feat: added tailwind css and WIP: first view of BoardList

--- importmap.php
+++ importmap.php
@@ -27,4 +27,9 @@ return [
     '@hotwired/stimulus' => ['version' => '3.2.2'],
     '@symfony/stimulus-bundle' => ['path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js'],
     '@hotwired/turbo' => ['version' => '8.0.23'],
+    'bootstrap' => ['version' => '5.3.8'],
+    '@popperjs/core' => ['version' => '2.11.8'],
+    'bootstrap/dist/css/bootstrap.min.css' => ['version' => '5.3.8', 'type' => 'css'],
+    'tailwindcss' => ['version' => '4.3.3'],
+    'tailwindcss/index.min.css' => ['version' => '4.3.3', 'type' => 'css'],
 ];
